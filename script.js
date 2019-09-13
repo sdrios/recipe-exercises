1890a26815b4007a11de9703ad78fc55ae349c32
Sarah <sarahd.rios@gmail.comm>
2019-09-12 21:20:40 -0500

    Commit recipe exercise

---
+++ script.js
@@ -0,0 +1,11 @@
+function marinaraSauce(cookName){
+    two = 2;
+    oneFourth = "1/4";
+    console.log("Hey, " + cookName +"! This is my marinara sauce recipe!");
+    console.log("- Heat a medium-large saucepan over medium heat.");
+    console.log("- Add " + 2 +" tspns of oil and 5 carlic cloves, cook until golden, about " + 2 +" minutes");
+    console.log("- Add " + oneFourth +" cup water, " + 2 +" cans of crushed tomates, and salt and season with black pepper to taste.");
+    console.log("- Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes");
+    console.log(" - Stir in " + oneFourth +" cup roughly chopped fresh basil, salt and pepper as needed.");
+}
+marinaraSauce("Adam");
